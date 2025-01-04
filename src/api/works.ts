b3eda71b625b552df624f3e30c2ef443e4c841ce
haimski <user@users-MacBook-Pro.local>
2025-01-04 17:51:03 +0200

    images urls update

--- src/api/works.ts
+++ src/api/works.ts
@@ -9,19 +9,19 @@ const works = [
         id: 2,
         title: "Vodafone Italy Retail App",
         description: "Retail application for a telecom company, for a agents and resellers, including a shop.",
-        imageUrl: "/public/works-gallery/RetailLogin.jpg"
+        imageUrl: "https://funtech.blog/personal-site-cdn/works-gallery/RetailLogin.jpg"
     },
     {
         id: 3,
         title: "METRO Prepaid Web App",
         description: "A Telecom company web application, made for agents to serve prepaid customers.",
-        imageUrl: "/public/works-gallery/metro-home.jpg"
+        imageUrl: "https://funtech.blog/personal-site-cdn/works-gallery/metro-home.jpg"
     },
     {
         id: 4,
         title: "Everything Everywhere Agents App",
         description: "Retail application made for agents to serve customers and sell products.",
-        imageUrl: "/public/works-gallery/ee_portal.jpg"
+        imageUrl: "https://funtech.blog/personal-site-cdn/works-gallery/ee_portal.jpg"
     }
 ];
 
