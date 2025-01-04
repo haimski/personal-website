f61298489690a00c78a3eb0e8cb4cd1c84820933
haimski <user@users-MacBook-Pro.local>
2025-01-04 17:27:00 +0200

    server config for images

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import handler from './src/sendMail.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,14 +18,8 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Disable caching for images
-app.use('src/assets', express.static(path.join(__dirname, 'images'), {
-    etag: false,
-    lastModified: false,
-    setHeaders: (res, path) => {
-        res.setHeader('Cache-Control', 'no-store');
-    }
-}));
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
     res.send('Welcome to the server!');
