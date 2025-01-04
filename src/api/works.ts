81f4fd8855813fbae3171088b66afade0ef2460e
haimski <user@users-MacBook-Pro.local>
2025-01-04 17:13:25 +0200

    setting images root

--- src/api/works.ts
+++ src/api/works.ts
@@ -3,25 +3,25 @@ const works = [
         id: 1,
         title: "Vodafone Italy Self service Portal",
         description: "Self Service portal for telecom customers, including a shop for deveices and accessories.",
-        imageUrl: "/src/assets/works-gallery/SS_Login.png"
+        imageUrl: "/public/works-gallery/SS_Login.png"
     },
     {
         id: 2,
         title: "Vodafone Italy Retail App",
         description: "Retail application for a telecom company, for a agents and resellers, including a shop.",
-        imageUrl: "/src/assets/works-gallery/RetailLogin.png"
+        imageUrl: "/public/works-gallery/RetailLogin.png"
     },
     {
         id: 3,
         title: "METRO Prepaid Web App",
         description: "A Telecom company web application, made for agents to serve prepaid customers.",
-        imageUrl: "/src/assets/works-gallery/metro-home.png"
+        imageUrl: "/public/works-gallery/metro-home.png"
     },
     {
         id: 4,
         title: "Everything Everywhere Agents App",
         description: "Retail application made for agents to serve customers and sell products.",
-        imageUrl: "/src/assets/works-gallery/ee_portal.png"
+        imageUrl: "/public/works-gallery/ee_portal.png"
     }
 ];
 
