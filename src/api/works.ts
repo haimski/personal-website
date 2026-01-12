a5b175bb4d4caa6ddb5b839139607e86a79e1d6a
haimski <benzak@gmail.com>
2026-01-12 09:12:44 +0200

    feat: Update image URL domain in works API

--- src/api/works.ts
+++ src/api/works.ts
@@ -3,25 +3,25 @@ const works = [
         id: 1,
         title: "Vodafone Italy Self service Portal",
         description: "Self Service portal for telecom customers, including a shop for deveices and accessories.",
-        imageUrl: "https://funtech.blog/personal-site-cdn/works-gallery/SS_Login.jpg"
+        imageUrl: "https://orchid-lyrebird-293063.hostingersite.com//personal-site-cdn/works-gallery/SS_Login.jpg"
     },
     {
         id: 2,
         title: "Vodafone Italy Retail App",
         description: "Retail application for a telecom company, for a agents and resellers, including a shop.",
-        imageUrl: "https://funtech.blog/personal-site-cdn/works-gallery/RetailLogin.jpg"
+        imageUrl: "https://orchid-lyrebird-293063.hostingersite.com//personal-site-cdn/works-gallery/RetailLogin.jpg"
     },
     {
         id: 3,
         title: "METRO Prepaid Web App",
         description: "A Telecom company web application, made for agents to serve prepaid customers.",
-        imageUrl: "https://funtech.blog/personal-site-cdn/works-gallery/metro-home.jpg"
+        imageUrl: "https://orchid-lyrebird-293063.hostingersite.com//personal-site-cdn/works-gallery/metro-home.jpg"
     },
     {
         id: 4,
         title: "Everything Everywhere Agents App",
         description: "Retail application made for agents to serve customers and sell products.",
-        imageUrl: "https://funtech.blog/personal-site-cdn/works-gallery/ee_portal.jpg"
+        imageUrl: "https://orchid-lyrebird-293063.hostingersite.com//personal-site-cdn/works-gallery/ee_portal.jpg"
     }
 ];
 
